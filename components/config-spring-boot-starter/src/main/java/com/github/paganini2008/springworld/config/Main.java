ed8452b31ce5b90eb52d02b2026d0fc46513eb18
Fred Feng <paganini2008@sina.com>
2020-02-13 20:19:59 +0800

    Commit

---
+++ components/config-spring-boot-starter/src/main/java/com/github/paganini2008/springworld/config/Main.java
@@ -0,0 +1,7 @@
+package com.github.paganini2008.springworld.config;
+
+public class Main {
+	
+	public static void main(String[] args) {
+	}
+}
---
+++ support/utils/src/main/java/com/github/paganini2008/springworld/support/TestMain.java
@@ -0,0 +1,9 @@
+package com.github.paganini2008.springworld.support;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+
+	}
+
+}
